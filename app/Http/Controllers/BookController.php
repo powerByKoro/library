b351002ff619fab46812c08d8a233bb67b05386a
PowerByKoro <brus200@yandex.ru>
2022-03-25 15:49:34 +0300

    Refactoring adding book to reservation

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reservation;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
@@ -21,21 +22,17 @@ class BookController extends Controller
 
     public function add(Request $request, $id): RedirectResponse
     {
+        /* @var $currentUser User */
         $currentUser = $request->user();
 
-        if (empty($basket_id)) {
-            // если корзина еще не существует — создаем объект
-            $basket = Reservation::create();
-            // получаем идентификатор, чтобы записать в cookie
-            $basket_id = $basket->id;
-        } else {
-            // корзина уже существует, получаем объект корзины
-            $basket = Reservation::findOrFail($basket_id);
-            // обновляем поле `updated_at` таблицы `baskets`
-            $basket->touch();
-        }
-        $basket->products()->attach($id);
-        // выполняем редирект обратно на страницу, где была нажата кнопка «В корзину»
-        return back()->withCookie(cookie('basket_id', $basket_id, 525600));
+        $reservation = new Reservation;
+        $reservation->user_id = $currentUser->id;
+        $reservation->save();
+
+        $reservation->books()->attach($id);
+
+        return back()->with([
+            'message' => 'Книга успешно добавлена!'
+        ]);
     }
 }
--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -3,12 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @property int id
+ * @property int user_id
+ * @property-read Book[] $books
+ */
 class Reservation extends Model
 {
     public function books(): BelongsToMany
     {
         return $this->belongsToMany(Book::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
