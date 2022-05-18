887c1aba27614aa5dfffda33233d37bf8a838a90
PowerByKoro <brus200@yandex.ru>
2022-05-18 18:27:32 +0300

    актуальный коммит 9

--- resources/views/information.blade.php
+++ resources/views/information.blade.php
@@ -13,7 +13,10 @@
         <h2 class="">Адрес: Волоколамское ш., 4, корп. 6, Москва</h2>
     </div>
     <div class="row">
-        <div class="col-5" style="margin-top: 20px"><script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A5842321da53758f43b55223b4f3f052c1e87dceb2c2270c1c2c70b41f5d1393f&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script></div>
+        <div class="col-5" style="margin-top: 20px">
+            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A5842321da53758f43b55223b4f3f052c1e87dceb2c2270c1c2c70b41f5d1393f&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true">
+
+            </script></div>
     </div>
 </div>
 
--- resources/views/send_email.blade.php
+++ resources/views/send_email.blade.php
@@ -5,10 +5,5 @@
                Ваш пароль : {{$password}} .
            </p>
         </div>
-        <div class="col-3">
-            <p class="">
-                Номер Вашего читательского билета : MAI-2022-{{$bilet}} .
-            </p>
-        </div>
     </div>
 </div>
