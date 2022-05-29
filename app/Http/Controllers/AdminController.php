ff90f70b79c8439d584c335efcc96bb1db7c7029
PowerByKoro <brus200@yandex.ru>
2022-05-29 21:01:45 +0300

    temp commit #6

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -40,7 +40,7 @@ class AdminController extends Controller
                 ->where('reservations.user_id', $user->id)
                 ->get();
 
-            if(!empty($reservationBooks)){
+            if($reservationBooks != null){
                 return view('reserved_books', compact('reservationBooks'));
             }else{
                 return back()->withErrors(['msg' => 'Резервов не найдено']);
