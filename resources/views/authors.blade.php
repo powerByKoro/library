1ac797ec737addbe6e6dec9d15965c167f43ac05
PowerByKoro <brus200@yandex.ru>
2022-05-29 21:31:38 +0300

    temp commit #6

--- resources/views/authors.blade.php
+++ resources/views/authors.blade.php
@@ -1,29 +1,36 @@
 
 
 <div class="container mt-3" style="border-top: 2px solid black">
-@foreach ($authors as $author)
-    <div class="row">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="row">
-                            <div class="col-6 offset-1">
-                                <h2 class="">{{$author->name}}</h2>
-                            </div>
-                            <div class="col-3 offset-1">
-                                <form action="/author/{{$author->id}}" method="post">
-                                    @csrf
-                                    <button class="btn btn-dark btn-md ">
-                                        Просмотреть книги автора
-                                    </button>
-                                </form>
+@if(count($authors))
+    @foreach ($authors as $author)
+        <div class="row">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-6 offset-1">
+                                    <h2 class="">{{$author->name}}</h2>
+                                </div>
+                                <div class="col-3 offset-1">
+                                    <form action="/author/{{$author->id}}" method="post">
+                                        @csrf
+                                        <button class="btn btn-dark btn-md ">
+                                            Просмотреть книги автора
+                                        </button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    @endforeach
+@else
+    <div class="col-3 offset-4">
+        <h2 style="" class="text-danger mt-5">Авторы не найдены</h2>
     </div>
-@endforeach
+
+@endif
 </div>
--- resources/views/reserved_books.blade.php
+++ resources/views/reserved_books.blade.php
@@ -1,25 +1,28 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-
-        @foreach($reservationBooks as $book)
-        <div class="row" style="border-bottom: 3px solid black">
-            <div class="col-3 mt-2">
-                   <h2>{{$book->name}}</h2>
-            </div>
-            <div class="col-3">
-                <h2>Дата брони: {{$book->created_at}}</h2>
-            </div>
-            <div class="col-3" >
-                <h2>Дата возврата: {{$book->date_return}}</h2>
-            </div>
-            <div class="col-3">
-                <form method="POST" action="/admin_dashboard_delete_book/{{$book->id}}" class="">
-                    @csrf
-                    <button class="btn-md btn-danger">Удалить резерв</button>
-                </form>
-            </div>
-        </div>
-        @endforeach
+    @if($reservationBooks)
+            @foreach($reservationBooks as $book)
+                <div class="row" style="border-bottom: 3px solid black">
+                    <div class="col-3 mt-2">
+                        <h2>{{$book->name}}</h2>
+                    </div>
+                    <div class="col-3">
+                        <h2>Дата брони: {{$book->created_at}}</h2>
+                    </div>
+                    <div class="col-3" >
+                        <h2>Дата возврата: {{$book->date_return}}</h2>
+                    </div>
+                    <div class="col-3">
+                        <form method="POST" action="/admin_dashboard_delete_book/{{$book->id}}" class="">
+                            @csrf
+                            <button class="btn-md btn-danger">Удалить резерв</button>
+                        </form>
+                    </div>
+                </div>
+            @endforeach
+    @else
+        <h2>Резервов не найдено</h2>
+    @endif
     </div>
 @endsection
