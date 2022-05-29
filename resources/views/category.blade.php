d137e4d9b2c57a72ac7fbc43e1b6977c5a4ccb21
PowerByKoro <brus200@yandex.ru>
2022-05-29 13:48:54 +0300

    temp commit #3

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -5,8 +5,10 @@
         <form action="/category/{{$category->id}}" method="post">
             @csrf
             <button class="list-group-item list-group-item-action">
-                <h4 class="">УДК - {{$category->YDK}}</h4>
-                <h4 class="">{{$category->name}}</h4>
+                <div class="row">
+                    <div class="col-3"> <h4 class="">УДК - {{$category->YDK}}</h4></div>
+                    <div class="col-8"><h4 class="">{{$category->name}}</h4></div>
+                </div>
             </button>
         </form>
     @endforeach
