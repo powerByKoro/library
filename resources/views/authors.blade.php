0f121b2f691852945b6ee0aea2b0c1992ad1c32d
PowerByKoro <brus200@yandex.ru>
2022-05-29 15:00:52 +0300

    temp commit #5

--- resources/views/authors.blade.php
+++ resources/views/authors.blade.php
@@ -6,29 +6,20 @@
         <div class="card">
             <div class="card-body">
                 <div class="row">
-                    <div class="col-4">
-                        <h2 class="">{{$author->name}}</h2>
-                        <form action="/author/{{$author->id}}" method="post">
-                            @csrf
-                            <button class="btn btn-dark btn-md mt-2">
-                                Просмотреть книги автора
-                            </button>
-                        </form>
-                    </div>
-                    <div class="col-3">
-                        <img src="{{asset('images/'.$author->image . '.jpg')}}" class="card-img-top" style="width: 250px; height: 250px; margin: 10px;border-radius: 10px">
-                    </div>
-                    <div class="col-3">
-                      <p class="" style="
-                        overflow: hidden;
-                          display: -webkit-box;
-                          -webkit-line-clamp: 9;
-                          -webkit-box-orient: vertical;
-                          line-height: 1.3em;
-                          height: 11.9em;
-                        ">
-                          {{$author->content}}
-                      </p>
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-4 offset-1">
+                                <h2 class="">{{$author->name}}</h2>
+                            </div>
+                            <div class="col-3 offset-2">
+                                <form action="/author/{{$author->id}}" method="post">
+                                    @csrf
+                                    <button class="btn btn-dark btn-md ">
+                                        Просмотреть книги автора
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
