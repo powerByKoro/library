3f3cf83e74c3ee04edf261778aebc8be16857650
PowerByKoro <brus200@yandex.ru>
2022-05-29 13:45:39 +0300

    temp commit #3

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -2,6 +2,12 @@
 
 <div class="list-group">
     @foreach ($categories as $category)
-        <a href="#" class="list-group-item list-group-item-action"><h4 class="">{{$category->YDK}}    -     {{$category->name}}</h4> </a>
+        <form action="/category/{{$author->id}}" method="post">
+            @csrf
+            <button class="list-group-item list-group-item-action">
+                <h4 class="">УДК - {{$category->YDK}}</h4>
+                <h4 class="">{{$category->name}}</h4>
+            </button>
+        </form>
     @endforeach
 </div>
